7f075e4f23236e37fb4dfabf42ee53be0a05279f
kcshubarna6 <kcshubarna6@gmail.com>
2024-09-11 01:30:54 -0400

    worked on header. Tried to make the drop-down div popup when the anchor tag gets clicked

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  divElementClub :any;
+  divElementTeam:any;
+  divElementResource:any;
+  ngOnInit(){
+    this.divElementTeam=document.querySelector(".team-manage-options");
+    this.divElementClub=document.querySelector(".club-manage-options");
+    this.divElementResource=document.querySelector(".resources-options");
+    
+
+  }
+
+  showTeamManageOption(){
+   
+
+   this.divElementClub.style.display = "none";
+   this.divElementResource.style.display = "none";
+
+    if (this.divElementTeam.style.display === "flex" ) {
+      this.divElementTeam.style.display = "none";
+      console.log("none")
+    } else {
+      this.divElementTeam.style.display = "flex";
+      console.log("flex")
+    }
+    console.log(this.divElementTeam);
+    
+  }
+  showClubManageOption(){
+    
+    
+   this.divElementTeam.style.display = "none";
+   this.divElementResource.style.display = "none";
+    if (this.divElementClub.style.display === "flex" ) {
+      this.divElementClub.style.display = "none";
+      console.log("none")
+    } else {
+      this.divElementClub.style.display = "flex";
+      console.log("flex")
+    }
+    console.log(this.divElementClub);
+
+  }
+ 
+  showResourceOption(){
+    this.divElementClub.style.display = "none";
+    this.divElementTeam.style.display = "none";
+    if (this.divElementResource.style.display === "flex" ) {
+      this.divElementResource.style.display = "none";
+      console.log("none")
+    } else {
+      this.divElementResource.style.display = "flex";
+      console.log("flex")
+    }
+
+  }
 
 }
